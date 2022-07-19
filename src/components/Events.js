6f6676e9baeeac8acaa2473eab0e5bec868fdf3f
Louis Kabore <louiskabore1998@gmail.com>
2022-07-19 23:24:00 +0200

    artist page inprogress

--- src/components/Events.js
+++ src/components/Events.js
@@ -3,11 +3,15 @@ import EventsData from "./mock-data.json"
 import { Link, useNavigate } from 'react-router-dom'
 
 import { BsFillCalendarDateFill } from 'react-icons/bs'
+
+import { MdOutlineLocationOn } from 'react-icons/md'
 // This holds a list of some fiction people
 // Some  have the same name but different age and id
 const EVENTS = EventsData;
 
 function SearchBar() {
+  const [showModal, setShowModal] = React.useState(false);
+
   // the value of the search field 
   const [event_title, setName] = useState('');
 
@@ -46,15 +50,15 @@ function SearchBar() {
         </div>
       </form>
 
-      <div className="flex flex-wrap -mx-1 lg:-mx-4">
+     
         {foundEvents && foundEvents.length > 0 ? (
           foundEvents.map((event) => (
-            <div>
-
+          
 
 
               <div key={event.id}>
-                
+              <div className="flex flex-wrap -mx-1 lg:-mx-4">
+
 
                     <div class="my-1 px-1 w-full md:w-1/2 lg:my-4 lg:px-4 lg:w-1/3">
                       <article class="overflow-hidden rounded-lg shadow-lg">
@@ -88,7 +92,7 @@ function SearchBar() {
                             <i class="fa fa-heart"></i>
                           </a>
                           <a class="relative py-2 px-3 text-xl font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-                            <Link to='/event'>Interested</Link>
+                            <button onClick={() => setShowModal(true)}>Interested</button>
                           </a>
                         </footer>
 
@@ -97,14 +101,119 @@ function SearchBar() {
 
                   </div>
                 </div>
-            
+             
 
           ))
         ) : (
           <h1 className='pt-3 text-2xl'> 🤯 No results found!</h1>
         )}
+
+
+        {/* Modal Box */}
+        {showModal ? (
+                        <>
+                            <div
+                                className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none backdrop-blur-md rounded-xl "
+                            >
+                                <div className="relative w-auto my-6 mx-auto max-w-3xl">
+                                    {/*content*/}
+                                    <div className="border-0 rounded-xl shadow-lg relative flex flex-col w-full bg-zinc-800 outline-none focus:outline-none ">
+                                        {/*header*/}
+                                        <div className="content bg-cover bg-center h-64  bg-[url('https://picsum.photos/600/400/?random')] rounded-xl">
+
+                                        </div>
+                                        <div className="text-center justify-between pt-4 border-b border-solid border-slate-200 rounded-xl">
+                                            <h3 className="text-3xl font-semibold text-white">
+                                                Title
+                                            </h3>
+                                            <button
+                                                className="p-1 ml-auto bg-transparent border-0 text-black opacity-5 float-right text-3xl leading-none font-semibold outline-none focus:outline-none"
+                                                onClick={() => setShowModal(false)}
+                                            >
+                                                <span className="bg-transparent text-black opacity-5 h-6 w-6 text-2xl block outline-none focus:outline-none">
+                                                    <p>×</p>
+                                                </span>
+                                            </button>
+                                        </div>
+                                        {/*body*/}
+
+                                        <div className="relative p-6 flex-auto">
+                                            <h1 className='text-2xl text-white font-bold'>Description</h1>
+                                            <p className="my-4 text-neutral-400 text-lg leading-relaxed">
+
+                                                I always felt like I could do anything. That’s the main
+                                                thing people are controlled by! Thoughts- their perception
+                                                of themselves! They're slowed down by their perception of
+                                                themselves. If you're taught you can’t do anything, you
+                                                won’t do anything. I was taught I could do everything.
+                                            </p>
+
+                                            
+                                            <h1 className='text-2xl text-white font-bold'>Line Up</h1>
+                                                <div class="my-5 p-4 flex">
+                                                    <div class="w-full flex justify-start space-x-2">
+                                                        <div className='text-center'>
+                                                        <Link to="/artist">
+                                                        <img class="w-20 h-20 rounded-full" src="https://picsum.photos/600/400/?random" alt=""></img>
+                                                        </Link>
+                                                        <span className='text-white'>Offset</span>
+                                                        </div>
+                                                        
+                                                        <img class="w-20 h-20 rounded-full" src="https://picsum.photos/600/400/?random" alt=""></img>
+                                                        <img class="w-20 h-20 rounded-full" src="https://picsum.photos/600/400/?random" alt=""></img>
+                                                    </div>
+                                                </div>
+                                          
+
+
+
+                                            <h1 className='text-2xl text-white font-bold'>Info</h1>
+
+                                            <div class="my-4 flex">
+                                                <div class="w-full flex justify-start space-x-1">
+                                                    <BsFillCalendarDateFill className='text-white text-xl ' />
+                                                    <p className='text-white pl-2 text-xl '>Saturday 16 july</p>
+
+                                                </div>
+                                                <br />
+                                            </div>
+
+                                            <div class="w-full flex justify-start space-x-1">
+                                                <MdOutlineLocationOn className='text-white text-2xl ' />
+                                                <p className='text-white pl-2 text-xl'>Paris</p>
+                                            </div>
+
+                                        </div>
+
+
+                                        <div className='p-6 flex space-x-2'>
+
+
+                                        </div>
+
+
+                                        {/*footer*/}
+                                        <div className="flex items-center justify-end p-6  rounded-b">
+                                            <button
+                                                className="text-red-500 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                                                type="button"
+                                                onClick={() => setShowModal(false)}
+                                            >
+                                                Close
+                                            </button>
+                                            <button class="text-xl inline-block py-2 px-2.5 leading-none text-center whitespace-nowrap align-baseline font-bold bg-amber-200 text-gray-700 rounded-xl">BOOK NOW FOR 55 €</button>
+
+
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="opacity-25 fixed inset-0 z-40 bg-black"></div>
+                        </>
+                    ) : null}
+
       </div>
-    </div>
+    
   );
 }
 
--- src/components/artists/Artist.jsx
+++ src/components/artists/Artist.jsx
@@ -1,29 +1,55 @@
 import React from 'react'
 import ReactPlayer from "react-player"
 
+import {GrSoundcloud} from 'react-icons/gr'
+
+
+
 const Artist = () => {
+
+    const handleClick = () => {
+        window.open("https://soundcloud.com/91666/696e-symphonie?utm_source=clipboard&utm_medium=text&utm_campaign=social_sharing");
+      };
+
     return (
-        <div className=''>
+        <div className="bg-cover bg-[url('https://i.pravatar.cc/300')] text-white ">
+        <div className='backdrop-blur-xl '>
             <h1 className='text-2xl text-black font-bold'>Line Up</h1>
 
-            <div class="flex flex-wrap justify-center">
-                <div class="w-6/12 sm:w-4/12 px-4">
-                    <img src="https://www.creative-tim.com/learning-lab/tailwind-starter-kit/img/team-2-800x800.jpg" alt="..." class="shadow rounded-full max-w-full h-auto align-middle border-none" />
-                    <h1 className='text-center text-6xl font-bold p-4'>Daft Punk</h1>
+            <div class="py-24 flex items-center justify-center space-x-8">
+   <div class="w-36 h-36 bg-[url('https://i.pravatar.cc/300')] rounded-full object-contain"></div>
+  
+</div>
+            <div className='text-center'> 
+                <h1 className='text-center text-8xl font-bold p-4'>Daft Punk</h1>
+                <button onClick={handleClick}> <GrSoundcloud class=" flex justify-center text-2xl "/> </button>
+           </div>
+
+            <div className='flex flex-wrap justify-center px-30 p-4 mt-8 '>
+                <div className=''>
+                    <h1 className='text-4xl font-bold uppercase'>👉 Next Event</h1>
+                   
+
+                
+                   <button onClick={handleClick}> <img class=" flex rounded-xl w-230 h-130 pt-6 " src="https://picsum.photos/600/400/?random" alt="..."/>
+                   <br/>
+                   <p className='text-2xl font-bold'>Event title</p>
+
+                   <div className='flex flex-start'>
+                     <p className='text-2xl'>Place</p>
+                    </div>
+
+                    
+                     <div className='flex inline'>
+                        <p className='text-2xl text-red-600'>Date</p>
+                        <p className='pl-4 text-2xl'>Price</p>
+                        
+                     </div>
+                     </button>
                 </div>
             </div>
 
-            <h1 className='text-4xl font-bold p-4 mt-8'>Bio</h1>
-
-
-            <h1 className='text-4xl font-bold p-4 mt-8'>Next Event</h1>
-
-            <div className='flex justify-center pt-16'>
-                <img class=" rounded-xl w-196 h-96 " src="https://picsum.photos/600/400/?random" alt="..."  />
-
             </div>
-
-
         </div>
 
     )
